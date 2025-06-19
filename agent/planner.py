a16e2c682cb6f3275891968a16ae9c87089f4f84
agnishwarr <agnishwar.raychaudhuri22@st.niituniversity.in>
2025-06-19 10:44:42 +0530

    Further changes

--- agent/planner.py
+++ agent/planner.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 from jinja2 import Template
 
